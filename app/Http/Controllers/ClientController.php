69530f402147a2cc51d348fe183749af810bcf20
Fukalov <andrey.fukalov@payoola.com>
2021-02-15 11:41:05 +0400

    fix default value days2

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -75,15 +75,15 @@ class ClientController extends Controller
             session(['clientid' =>  $client->clientid]);
             return view('customer', [
                 'qr_auth' => 1,
-                'check_day_start' => env('CHECK_DAY_START'),
-                'check_day_end' => env('CHECK_DAY_END'),
+                'check_day_start' => env('CHECK_DAY_START',20),
+                'check_day_end' => env('CHECK_DAY_END',25),
             ]);
         }
         else {
             return view('customer', [
                 'qr_auth' => 0,
-                'check_day_start' => env('CHECK_DAY_START'),
-                'check_day_end' => env('CHECK_DAY_END'),
+                'check_day_start' => env('CHECK_DAY_START',20),
+                'check_day_end' => env('CHECK_DAY_END',25),
             ]);
         }
     }
