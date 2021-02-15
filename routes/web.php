5200a6d40e2dc1b9cb30e53986bd6a4d107cd037
Fukalov <andrey.fukalov@payoola.com>
2021-02-15 12:00:33 +0400

    fix date

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('about', function () {
 Route::get('customer', function () {
     return view('customer', [
         'qr_auth' => 0,
-        'check_day_start' => env('CHECK_DAY_START', 20),
+        'check_day_start' => env('CHECK_DAY_START', 15),
         'check_day_end' => env('CHECK_DAY_END', 25),
         ]);
 });
