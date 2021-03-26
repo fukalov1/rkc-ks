93f393bbde2c21614e9567f3ae63e83d1633c04e
Fukalov <andrey.fukalov@payoola.com>
2021-03-27 00:44:38 +0400

    add slips3

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -194,7 +194,7 @@ class ClientController extends Controller
         $id = session('clientid');
         if ($id) {
             $slips = $this->rkc_kvitki
-                ->select(`ls_date`, `ls`, `date`, `year`, `month`, `date_txt`, `dwltime`)
+                ->select('ls_date', 'ls', 'date', 'year', 'month', 'date_txt', 'dwltime')
                 ->where('ls', $id)
                 ->orderByDesc('year')
                 ->orderByDesc('month')
